e6d2e0ba770cfb47d20170681e75f6da7403e5d6
Manjula36 <manjulavdt@gmail.com>
2024-01-02 16:11:07 +0530

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp1
+{
+    internal class Program
+    { static class college Extending  {public static void 
+    postgrad(this college c) 
+        { Console.WriteLine("post graduation is available for new students"); } }
+    static void Main()
+    { College c = new College();
+        c.Tenth(); c.Plus2(); c.Graduation(); c.postgrad();
+    }     
+
---
+++ ServiceBank/ServiceBank/Bank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBank
+{
+    public  class Bank
+    {public void savings() { Console.WriteLine("savings"); }
+       public void current() { Console.WriteLine("current"); }
+        public void Recurring() { Console.WriteLine("Recurring"); }
+    }
+}
